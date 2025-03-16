f5ef9fe66ae6017097bdbaac2da8766a42ce2030
Ivan Ganev <iganev@cytec.bg>
2025-03-16 17:33:41 -0600

    wip

--- src/bin/sandbox.rs
+++ src/bin/sandbox.rs
@@ -1,10 +1,11 @@
-use std::str::FromStr;
+use std::{path::PathBuf, process::Stdio, str::FromStr};
 
-use babypi::rpicam::Rpicam;
-use tracing::info;
+use babypi::rpicam::{Rpicam, RpicamCodec, RpicamDeviceMode, RPICAM_BIN};
+use tokio::process::Command;
+use tracing::{error, info};
 use tracing_subscriber::{util::SubscriberInitExt, FmtSubscriber};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,9 +16,118 @@ async fn main() -> Result<()> {
         .finish()
         .init();
 
-    let res = Rpicam::list_cameras().await?;
+    // let res = Rpicam::list_cameras().await?;
 
-    info!("Devices: {:?}", res);
+    // let dev = res
+    //     .get(0)
+    //     .ok_or_else(|| anyhow!("No devices found"))?
+    //     .clone();
+
+    let tuning_file = PathBuf::from_str("/usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json")
+        .map_err(|e| anyhow!("Failed to locate tuning file: {}", e))?;
+
+    let mut cam = Rpicam::new(
+        None,
+        Some(RpicamCodec::default()),
+        None,
+        Some(tuning_file),
+        None,
+        None,
+    )
+    .spawn()
+    .await?;
+
+    let mut stdout = cam
+        .stdout
+        .take()
+        .ok_or_else(|| anyhow!("Failed to capture child process output for {}", RPICAM_BIN))?;
+
+    // let stderr = child.stderr.take().ok_or_else(|| {
+    //     anyhow!(
+    //         "Failed to capture child process err output for {}",
+    //         RPICAM_BIN
+    //     )
+    // })?;
+
+    // let mut reader = BufReader::new(stderr).lines();
+
+    tokio::spawn(async move {
+        match cam.wait().await {
+            Ok(code) => {
+                info!(
+                    "Child process {} exit code: {}",
+                    RPICAM_BIN,
+                    code.code().unwrap_or(-1)
+                );
+            }
+            Err(e) => {
+                error!("Child process {} error: {}", RPICAM_BIN, e);
+            }
+        }
+    });
+
+    //
+
+    //ffmpeg -y \
+    //   -probesize 32M \
+    //   -thread_queue_size 256 \
+    //   -use_wallclock_as_timestamps 1 \
+    //   -i live.h264 \
+    //   -c:v copy \
+    //   -f segment \
+    //   -segment_time 4 \
+    //   -segment_format mpegts \
+    //   -segment_list "/var/stream/live.m3u8" \
+    //   -segment_list_size 8 \
+    //   -segment_list_flags live \
+    //   -segment_list_type m3u8 \
+    //   -segment_wrap 10 \
+    //   "/var/stream/%08d.ts"
+
+    let ffmpeg_args = [
+        "-y",
+        "-probesize 32M",
+        "-thread_queue_size 256",
+        "-use_wallclock_as_timestamps 1",
+        "-i pipe:",
+        "-c:v copy",
+        "-f segment",
+        "-segment_time 4",
+        "-segment_format mpegts",
+        "-segment_list \"/var/stream/live.m3u8\"",
+        "-segment_list_size 8",
+        "-segment_list_flags live",
+        "-segment_list_type m3u8",
+        "-segment_wrap 10",
+        "\"/var/stream/%08d.ts\"",
+    ];
+
+    let mut ffmpeg = Command::new("ffmpeg")
+        .args(&ffmpeg_args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    if let Some(mut ffmpeg_stdin) = ffmpeg.stdin.take() {
+        tokio::spawn(async move {
+            tokio::io::copy(&mut stdout, &mut ffmpeg_stdin).await.ok();
+        });
+    }
+
+    tokio::spawn(async move {
+        match ffmpeg.wait().await {
+            Ok(code) => {
+                info!(
+                    "Child process {} exit code: {}",
+                    "ffmpeg",
+                    code.code().unwrap_or(-1)
+                );
+            }
+            Err(e) => {
+                error!("Child process {} error: {}", "ffmpeg", e);
+            }
+        }
+    });
 
     Ok(())
 }
--- src/rpicam.rs
+++ src/rpicam.rs
@@ -1,5 +1,7 @@
+use std::fmt::Display;
 use std::path::PathBuf;
 use std::process::Stdio;
+use std::str::FromStr;
 use std::sync::LazyLock;
 
 use anyhow::anyhow;
@@ -7,6 +9,7 @@ use anyhow::Result;
 use regex::Regex;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::BufReader;
+use tokio::process::Child;
 use tokio::process::Command;
 use tracing::error;
 use tracing::info;
@@ -31,42 +34,60 @@ pub static RPICAM_LIST_REGEX_MODE_FORMAT_CONTINUE_REGEX: LazyLock<Regex> = LazyL
         .expect("Failed to compile mode format continue regex")
 });
 
-//
-// rpicam-vid -t 0 -n --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json --codec h264 --framerate 30 --width 1920 --height 1080 --inline --listen -o - | psips > live.h264
-//
 #[derive(Clone, Debug)]
 pub struct Rpicam {
     pub camera: Option<RpicamDevice>,
-    pub tuning_file: Option<PathBuf>,
     pub codec: Option<RpicamCodec>,
-    pub framerate: u32,
-    pub width: u32,
-    pub height: u32,
-    pub psips_pipe: bool,
+    pub mode: Option<RpicamDeviceMode>,
+    pub tuning_file: Option<PathBuf>,
+    pub output_file: Option<PathBuf>,
     pub extra_args: Option<Vec<String>>,
+    // pub psips_pipe: bool,
 }
 
 impl Default for Rpicam {
     fn default() -> Self {
         Self {
             camera: None,
-            tuning_file: None,
             codec: Default::default(),
-            framerate: 30,
-            width: 1920,
-            height: 1080,
-            psips_pipe: true,
+            mode: Default::default(),
+            tuning_file: None,
+            output_file: None,
             extra_args: None,
+            // psips_pipe: true,
         }
     }
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub enum RpicamCodec {
     #[default]
     H264,
     MJPEG,
     YUV420,
+}
+
+impl Display for RpicamCodec {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RpicamCodec::H264 => write!(f, "h264"),
+            RpicamCodec::MJPEG => write!(f, "mjpeg"),
+            RpicamCodec::YUV420 => write!(f, "yuv420"),
+        }
+    }
+}
+
+impl FromStr for RpicamCodec {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "h264" => Ok(Self::H264),
+            "mjpeg" => Ok(Self::MJPEG),
+            "yuv420" => Ok(Self::YUV420),
+            _ => Err(anyhow!("Unknown codec: {}", s)),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Default)]
@@ -107,12 +128,23 @@ impl RpicamDevice {
     }
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 pub struct RpicamDeviceMode {
     pub format: String,
     pub width: u32,
     pub height: u32,
     pub fps: u32,
+}
+
+impl Default for RpicamDeviceMode {
+    fn default() -> Self {
+        Self {
+            format: "default".to_string(),
+            width: 1920,
+            height: 1080,
+            fps: 30,
+        }
+    }
 }
 
 impl RpicamDeviceMode {
@@ -241,7 +273,7 @@ impl Rpicam {
                     return Err(anyhow!("Failed to parse device information: {}", full));
                 }
             } else {
-                println!("Unparsed Line: {}", line);
+                // info!("Unparsed Line: {}", line);
             }
         }
 
@@ -251,5 +283,119 @@ impl Rpicam {
         }
 
         Ok(results)
+    }
+
+    pub fn new(
+        camera: Option<RpicamDevice>,
+        codec: Option<RpicamCodec>,
+        mode: Option<RpicamDeviceMode>,
+        tuning_file: Option<PathBuf>,
+        output_file: Option<PathBuf>,
+        extra_args: Option<Vec<String>>,
+        // psips: bool,
+    ) -> Self {
+        Self {
+            camera,
+            codec,
+            mode,
+            tuning_file,
+            output_file,
+            extra_args,
+            // psips_pipe: psips,
+        }
+    }
+
+    //
+    // rpicam-vid -t 0 -n --tuning-file /usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json --codec h264 --framerate 30 --width 1920 --height 1080 --inline --listen -o - | psips > live.h264
+    //
+    fn build_rpicam_cmd_args(&self) -> Vec<String> {
+        let mut args = Vec::new();
+        args.push("-t 0".to_string());
+        args.push("-n".to_string());
+
+        if let Some(tuning_file) = self.tuning_file.as_deref() {
+            args.push(format!("--tuning-file {}", tuning_file.to_string_lossy()));
+        }
+
+        if let Some(codec) = &self.codec {
+            args.push(format!("--codec {}", codec));
+
+            if *codec == RpicamCodec::H264 {
+                args.push("--inline".to_string());
+            }
+        }
+
+        let (w, h, fps) = if let Some(mode) = self.mode.as_ref() {
+            (mode.width, mode.height, mode.fps)
+        } else {
+            let mode = RpicamDeviceMode::default();
+            (mode.width, mode.height, mode.fps)
+        };
+
+        args.push(format!("--framerate {}", fps));
+        args.push(format!("--width {}", w));
+        args.push(format!("--height {}", h));
+
+        let output = self
+            .output_file
+            .as_deref()
+            .and_then(|p| p.to_str())
+            .unwrap_or("-");
+
+        args.push(format!("-o {}", output));
+
+        args
+    }
+
+    pub async fn spawn(&self) -> Result<Child> {
+        let args = self.build_rpicam_cmd_args();
+
+        info!("CMD ARGS: {:?}", args);
+
+        let child = Command::new(RPICAM_BIN)
+            .args(&args)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .map_err(|e| anyhow!("Failed to spawn child process {}: {}", RPICAM_BIN, e))?;
+
+        let Some(_pid) = child.id() else {
+            return Err(anyhow!(
+                "Failed to resolve child process PID for {}",
+                RPICAM_BIN
+            ));
+        };
+
+        // let stdout = child
+        //     .stdout
+        //     .take()
+        //     .ok_or_else(|| anyhow!("Failed to capture child process output for {}", RPICAM_BIN))?;
+
+        // let stderr = child.stderr.take().ok_or_else(|| {
+        //     anyhow!(
+        //         "Failed to capture child process err output for {}",
+        //         RPICAM_BIN
+        //     )
+        // })?;
+
+        // let mut reader = BufReader::new(stderr).lines();
+
+        // tokio::spawn(async move {
+        //     match child.wait().await {
+        //         Ok(code) => {
+        //             info!(
+        //                 "Child process {} exit code: {}",
+        //                 RPICAM_BIN,
+        //                 code.code().unwrap_or(-1)
+        //             );
+        //         }
+        //         Err(e) => {
+        //             error!("Child process {} error: {}", RPICAM_BIN, e);
+        //         }
+        //     }
+        // });
+
+        Ok(child)
     }
 }
