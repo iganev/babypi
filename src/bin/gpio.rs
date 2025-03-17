78816584939b5294abb51037475543028acc6716
Ivan Ganev <iganev@cytec.bg>
2025-03-17 16:32:30 -0600

    wip

--- src/bin/gpio.rs
+++ src/bin/gpio.rs
@@ -44,7 +44,7 @@ async fn main() -> Result<()> {
 
         if let Some(index) = buf.iter().position(|b| *b == '\n' as u8) {
             println!("UART: {}", String::from_utf8_lossy(&buf[0..index]));
-            buf.copy_within(index + 0.., 0);
+            buf.copy_within((index + 1).., 0);
             offset = 0;
         } else if offset == 255 {
             println!("UART: {}", String::from_utf8_lossy(&buf[0..]));
