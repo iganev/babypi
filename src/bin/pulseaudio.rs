b19cfd6970a01fac3e0b801442ac6b8037e7153a
Ivan Ganev <iganev@cytec.bg>
2025-03-23 19:25:53 -0600

    wip

---
+++ src/bin/pulseaudio.rs
@@ -0,0 +1,44 @@
+use anyhow::anyhow;
+use anyhow::Result;
+
+use libpulse_binding as pulse;
+use libpulse_simple_binding as simple;
+use std::fs::File;
+use std::io::Write;
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let spec = pulse::sample::Spec {
+        format: pulse::sample::Format::S16le,
+        channels: 1,
+        rate: 48000,
+    };
+
+    let s = simple::Simple::new(
+        None,
+        "rust_recorder",
+        pulse::stream::Direction::Record,
+        Some("alsa_input.usb-DCMT_Technology_USB_Lavalier_Microphone_214b206000000178-00.mono-fallback"),
+        "record",
+        &spec,
+        None,
+        None,
+    )?;
+
+    let buffer_size = 1024 * 8;
+    let mut buffer = vec![0u8; buffer_size];
+
+    let mut file = File::create("test.raw")?;
+
+    let duration_secs = 60;
+    let iterations = (duration_secs * spec.rate as i32) / (buffer_size as i32 / 2);
+
+    for _ in 0..iterations {
+        s.read(&mut buffer)?;
+        file.write_all(&buffer)?;
+    }
+
+    println!("Recording finished. Saved to test.raw");
+
+    Ok(())
+}
